7bb865195df92cfaccfd0d5f98c1a54cc12ed83d
Erica Ayala <erica.ayala.work@gmail.com>
2023-09-03 14:43:38 -0400

    Remove unused commented out code and change name of initialized game.

--- src/rockpaperscissors/Game.java
+++ src/rockpaperscissors/Game.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 public class Game implements PlayGame {
     public static void main(String[] args) {
 
-        Game game1 = new Game();
+        Game game = new Game();
 
         ArrayList<String> winningCombos = new ArrayList<>();
         winningCombos.add(String.valueOf(new ArrayList<String>(Arrays.asList("Rock", "Scissors"))));
@@ -22,7 +22,7 @@ public class Game implements PlayGame {
         losingCombos.add(String.valueOf(new ArrayList<String>(Arrays.asList("Scissors", "Rock"))));
 
 
-        game1.startIntro(winningCombos, losingCombos);
+        game.startIntro(winningCombos, losingCombos);
 
     }
 
@@ -73,7 +73,6 @@ public class Game implements PlayGame {
             getOpponentChoice(player1, player2, winningCombos, losingCombos);
 
         }
-
     }
 
 
@@ -89,25 +88,6 @@ public class Game implements PlayGame {
 
             Computer computer = new Computer(0, 0, 0, "", "Computer");
 
-            /*
-
-            ArrayList<String> possibleChoices = new ArrayList<>();
-            possibleChoices.add("Rock");
-            possibleChoices.add("Paper");
-            possibleChoices.add("Scissors");
-
-            Random random = new Random();
-            // Sets the upper bound to generate random numbers in specific range
-            int upperbound = 3;
-            // Generates random values from 0 - 2 using nextInt()
-            int randomNum = random.nextInt(upperbound);
-
-            String computerChoice = possibleChoices.get(randomNum);
-            computer.setChoice(computerChoice);
-            System.out.println("Computer chose " + computerChoice);
-
-             */
-
             String computerChoice = computer.makeRandomChoice();
 
             computer.setChoice(computerChoice);
@@ -118,23 +98,6 @@ public class Game implements PlayGame {
 
             // Make currentGameStats a List/Collection and use a lambda expression to stream/map contents to gameHistory before clearing currentGameStats
             ArrayList<Object> gameHistory = new ArrayList<>();
-
-            /*
-
-            ArrayList<String> winningCombos = new ArrayList<>();
-            winningCombos.add(String.valueOf(new ArrayList<String>(Arrays.asList("Rock", "Scissors"))));
-            winningCombos.add(String.valueOf(new ArrayList<String>(Arrays.asList("Paper", "Rock"))));
-            winningCombos.add(String.valueOf(new ArrayList<String>(Arrays.asList("Scissors", "Paper"))));
-
-            ArrayList<String> losingCombos = new ArrayList<>();
-            losingCombos.add(String.valueOf(new ArrayList<String>(Arrays.asList("Rock", "Paper"))));
-            losingCombos.add(String.valueOf(new ArrayList<String>(Arrays.asList("Paper", "Scissors"))));
-            losingCombos.add(String.valueOf(new ArrayList<String>(Arrays.asList("Scissors", "Rock"))));
-
-             */
-
-
-            // if ( currentGameStats.equalsIgnoreCase(winningCombos[0]) || currentGameStats.equalsIgnoreCase(winningCombos[1]) || currentGameStats.equalsIgnoreCase(winningCombos[2]) ) {
 
 
             if (currentGameStats.get(0).equalsIgnoreCase(winningCombos.get(0)) || currentGameStats.get(0).equalsIgnoreCase(winningCombos.get(1)) || currentGameStats.get(0).equalsIgnoreCase(winningCombos.get(2))) {
